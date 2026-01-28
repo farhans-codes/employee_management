898c89c64c6b81761fa3e3f321da2ea61fa1ab30
DM IRD <108507343+farhans-codes@users.noreply.github.com>
2026-01-28 13:23:52 +0600

    refactor(core): remove firebase & supabase 🚮
    - BREAKING CHANGE: Removed Firebase and Supabase dependencies

--- lib/core/config/supabase_config.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseConfig {
-  static const String supabaseUrl = 'https://cxomejtuwayublxjbdxg.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4b21lanR1d3lhdWJseGpiZHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1Mjc5NTQsImV4cCI6MjA4NTEwMzk1NH0.uvDs-yfxK1dnxwC7rHAYQ8TJm7ZhB_4FqSUmztAFaVQ';
-}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import app_links
-import cloud_firestore
 import connectivity_plus
-import firebase_auth
-import firebase_core
 import package_info_plus
 import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
